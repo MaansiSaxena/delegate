dbeaa043e21d998b732cd5f9e0b01cd12f18a661
Mansi Saxena <Mansi.Saxena@bhavnacorp.com>
2022-09-01 15:24:15 +0530

    first commit

---
+++ delegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace delegate
+{
+    class Program
+    {
+        public delegate void addDelegate(int a, int b);
+        public delegate void greetingDelegate(string name);
+
+        public void add (int x, int y)
+        {
+            Console.WriteLine(@"The sume of {0} and {1} is {2},x y,(x+y)");
+        }
+        public static string Greetings(string name)
+        {
+            return "Hello" + name;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            addDelegate ad = new addDelegate(obj.add);
+            greetingDelegate gd = new greetingDelegate(Program.Greetings);
+        //invoking 
+            ad.invoke(100, 200);
+            string s = gd("mansi");
+            Console.WriteLine(s);
+
+        }
+    }
+}
